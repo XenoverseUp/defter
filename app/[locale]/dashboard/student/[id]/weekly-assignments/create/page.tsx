62a4b8d7e5dfb5db9b0aa904d543e60cb2923ab6
XenoverseUp <xenoverseup@gmail.com>
2025-07-19 01:17:38 +0300

    feat: calendar assignment view

--- app/[locale]/dashboard/student/[id]/weekly-assignments/create/page.tsx
+++ app/[locale]/dashboard/student/[id]/weekly-assignments/create/page.tsx
@@ -1,33 +1,30 @@
-import AssignmentCalendar from "@/components/ui/assignment-calendar";
+"use client";
+
+import AssignmentCalendar, { type Assignment } from "@/components/ui/assignment-calendar";
 import { Button } from "@/components/ui/button";
-import { PencilRulerIcon, PlusCircleIcon } from "lucide-react";
-// import { Button } from "@/components/ui/button";
-// import { Calendar, CalendarCurrentDate, CalendarWeekView } from "@/components/ui/full-calendar";
-// import { PencilRulerIcon, PlusCircleIcon } from "lucide-react";
+import { NotebookIcon, PencilRulerIcon, PlusCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function CreateAssignments() {
+  const [assignments, setAssignments] = useState<Assignment[]>([]);
+
   return (
-    <>
-      <div className="py-6 flex flex-col">
-        <header className="flex px-6 items-center gap-4 mb-6">
-          <div>
-            <h2 className="text-lg font-medium flex items-center gap-1.5">
-              <PencilRulerIcon className="size-5" strokeWidth={1.5} />
-              Weekly Schedule
-            </h2>
-          </div>
-
-          <p className="ml-auto text-sm font-medium">July 2025</p>
-          <Button size="sm" variant="secondary">
-            <PlusCircleIcon />
-            Create Assignment
-          </Button>
-        </header>
-
-        <div className="overflow-auto relative">
-          <AssignmentCalendar />
+    <div className="py-6 flex flex-col gap-6">
+      <header className="flex px-6 items-center gap-4">
+        <div>
+          <h2 className="text-lg font-medium flex items-center gap-1.5">
+            <PencilRulerIcon className="size-5" strokeWidth={1.5} />
+            Weekly Schedule
+          </h2>
         </div>
-      </div>
-    </>
+
+        <p className="ml-auto text-sm font-medium">July 2025</p>
+        <Button size="sm">
+          <PlusCircleIcon /> Assign
+        </Button>
+      </header>
+
+      <AssignmentCalendar {...{ assignments, setAssignments }} />
+    </div>
   );
 }
--- components/ui/assignment-calendar.tsx
+++ components/ui/assignment-calendar.tsx
@@ -1,12 +1,21 @@
 import { cn } from "@/lib/utils";
+import type { UUID } from "crypto";
+import { Button } from "./button";
+import { CalendarDaysIcon, PlusIcon } from "lucide-react";
+import { Dialog, DialogHeader, DialogTitle, DialogTrigger, DialogContent, DialogDescription } from "@/components/ui/dialog";
 
-type Assignment = {};
+export type Assignment = {
+  day: number;
+  questionCount: number;
+  resourceId: UUID | string;
+};
 
 interface Props {
   assignments?: Assignment[];
+  setAssignments: (v: Assignment[]) => void;
 }
 
-export default function AssignmentCalendar({ assignments }: Props) {
+export default function AssignmentCalendar({ assignments = [], setAssignments }: Props) {
   const currentWeekDates = Array.from(Array(7).keys()).map((idx) => {
     const d = new Date();
     d.setDate(d.getDate() - ((d.getDay() + 6) % 7) + idx);
@@ -14,7 +23,7 @@ export default function AssignmentCalendar({ assignments }: Props) {
   });
 
   return (
-    <div className="w-full min-h-32 grid grid-cols-7 grid-rows-1 rounded-t-lg divide-x border border-b-0">
+    <div className="w-full min-h-96 grid grid-cols-7 grid-rows-1 rounded-t-lg divide-x border border-b-0">
       {currentWeekDates.map((date, i) => (
         <DayView key={`day-view-${i}`} index={i} date={date} />
       ))}
@@ -22,8 +31,10 @@ export default function AssignmentCalendar({ assignments }: Props) {
   );
 }
 
-function DayView({ index, date }: { index: number; date: Date }) {
-  const { format } = new Intl.DateTimeFormat("en-US", { weekday: "short" });
+function DayView({ index, date, items = [] }: { index: number; date: Date; items?: Assignment[] }) {
+  const short = new Intl.DateTimeFormat("en-US", { weekday: "short" });
+  const long = new Intl.DateTimeFormat("en-US", { day: "numeric", weekday: "long", year: "numeric" });
+
   if (index >= 7) return;
 
   const isToday = (() => {
@@ -38,13 +49,41 @@ function DayView({ index, date }: { index: number; date: Date }) {
           "text-muted-foreground/50": [5, 6].includes(index),
         })}
       >
-        <span>{format(date)}</span>
-        <span className={cn("rounded-full size-6 flex items-center justify-center", { "bg-sky-500 ml-1.5 text-white": isToday })}>
+        <span>{short.format(date)}</span>
+        <span
+          className={cn("rounded-full size-6 flex items-center justify-center", {
+            "bg-neutral-900/75 ml-1.5 text-white text-xs font-medium": isToday,
+          })}
+        >
           {date.getDate()}
         </span>
       </header>
-      <div className="flex flex-col gap-2 px-2 pt-2">
-        <Entry />
+      <div
+        className={cn("flex flex-col gap-2 px-2 pt-2 h-full", {
+          "bg-linear-180 from-muted/50 from-95% to-transparent": [5, 6].includes(index),
+        })}
+      >
+        {items.map((item) => (
+          <Entry key={item.resourceId} />
+        ))}
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm" className="shadow-none">
+              <PlusIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="p-6 border-none!">
+            <DialogHeader>
+              <DialogTitle className="flex items-center gap-1.5">
+                <CalendarDaysIcon /> {long.format(date)}
+              </DialogTitle>
+              <DialogDescription>Create resource in student profile to track their progress.</DialogDescription>
+            </DialogHeader>
+
+            {/* <CreateResourceForm {...{ grade, studentId, setOpen }} /> */}
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
